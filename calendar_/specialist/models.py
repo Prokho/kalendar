43bd6bffb0527f022a5e308dcb6f03132507238f
Prokhor Proshkin <pproshkin@yandex.ru>
2023-01-12 20:38:56 +0300

    complited backend at 12/01/2023

--- calendar_/specialist/models.py
+++ calendar_/specialist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class ModeJobSpecialist(models.Model):
+    specialist = models.ForeignKey(User, on_delete=models.RESTRICT)
+    time_begin = models.TimeField()
+    time_end = models.TimeField()
+    duration_job_minutes = models.IntegerField()
+    duration_brake_minutes = models.IntegerField()
+    future_job_days = models.IntegerField(default = 90)
+
+
+
+
+
